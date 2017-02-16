97cbebb27620787505f792108c7906589f0078a1
John Moore <jawnmoore@gmail.com>
2017-02-16 14:54:58 -0600

    Added focus and blur motion for both input fields

--- js/site.js
+++ js/site.js
@@ -6,6 +6,24 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
+  $('#name').on('focus', function() {
+    $('#input-name label').addClass('active');
+  });
+  $('#name').on('blur', function() {
+    if($('#name').val().length == 0){
+      $('#input-name label').removeClass('active');
+    }
+  });
+
+  $('#email').on('focus', function() {
+    $('#input-email label').addClass('active');
+  });
+  $('#email').on('blur', function() {
+    if($('#email').val().length == 0){
+      $('#input-email label').removeClass('active');
+    }
+  });
+
   $('#email').on('keyup', function() {
     var currentValue = $(this).val();
     var validPattern = $(this).attr('pattern');
